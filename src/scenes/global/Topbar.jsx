60741a9890e48f8de61622752d5c6ce936af13bd
Lucy Veron <lucymveron@gmail.com>
2023-01-25 17:57:42 +0100

    Toplbar icons

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -18,8 +18,30 @@ function Topbar() {
       {/* Search Bar */}
       <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
       </Box>
-      <IconButton></IconButton>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === 'dark'
+            ? (<DarkModeOutlinedIcon />)
+            : (<LightModeOutlinedIcon />)
+          }
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+
     </Box >
   )
 }

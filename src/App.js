47afd585788e0db1ea91cbbe481e94017a0836ce
Lucy Veron <lucymveron@gmail.com>
2023-01-27 19:37:12 +0100

    Contacts page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Sidebar from './scenes/global/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
+import Contacts from './scenes/contacts';
+// import Invoices from './scenes/invoices';
+// import Form from './scenes/form';
+// import Bar from './scenes/bar';
+// import Pie from './scenes/pie';
+// import Line from './scenes/line';
+// import Faq from './scenes/faq';
+// import Geography from './scenes/geography';
+// import Calendar from './scenes/calendar';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -20,19 +29,16 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-            </Routes>
-            {/* 
-              
               <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<Faq />} />
               <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} />
-             */}
+              <Route path="/calendar" element={<Calendar />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>

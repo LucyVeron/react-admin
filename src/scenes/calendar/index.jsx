7f7e5a33833ce6332c0de13f362ee6cac34c5833
Lucy Veron <lucymveron@gmail.com>
2023-01-28 12:46:58 +0100

    Calendar

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import FullCalendar, { formatDate } from '@fullcalendar/react';
+import FullCalendar from '@fullcalendar/react';
+import { formatDate } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
@@ -38,6 +39,83 @@ function Calendar() {
     return (
         <Box m="20px">
             <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
+            <Box display="flex" justifyContent="space-between">
+                {/* CALENDAR SIDEBAR */}
+                <Box
+                    flex="1 1 20%"
+                    backgroundColor={colors.primary[400]}
+                    p="15px"
+                    borderRadius="4px">
+                    <Typography variant="h5">Events</Typography>
+                    <List>
+                        {currentEvents.map((event) => {
+                            return (
+                                <ListItem
+                                    key={event.id}
+                                    sx={{
+                                        backgroundColor: colors.greenAccent[500],
+                                        margin: "10px 0",
+                                        borderRadius: "2px"
+                                    }}>
+                                    <ListItemText
+                                        primary={event.title}
+                                        secondary={
+                                            <Typography>
+                                                {formatDate(
+                                                    event.start,
+                                                    {
+                                                        year: "numeric",
+                                                        month: "short",
+                                                        day: "numeric"
+                                                    }
+                                                )}
+                                            </Typography>
+                                        }
+                                    />
+                                </ListItem>
+                            )
+                        })}
+                    </List>
+                </Box>
+
+                {/* CALENDAR */}
+                <Box flex="1 1 100%" ml="15px">
+                    <FullCalendar
+                        height="75vh"
+                        plugins={[
+                            dayGridPlugin,
+                            timeGridPlugin,
+                            interactionPlugin,
+                            listPlugin
+                        ]}
+                        headerToolbar={{
+                            left: "prev, next today",
+                            center: "title",
+                            right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+                        }}
+                        initialView="dayGridMonth"
+                        editable={true}
+                        selectable={true}
+                        selectMirror={true}
+                        dayMaxEvents={true}
+                        select={handleDateClick}
+                        eventClick={handleEventClick}
+                        eventsSet={(events) => setCurrentEvents(events)}
+                        initialEvents={[
+                            {
+                                id: "1234",
+                                title: "All-day event",
+                                date: "2023-01-21"
+                            },
+                            {
+                                id: "4321",
+                                title: "Timed event",
+                                date: "2023-01-28"
+                            },
+                        ]}
+                    />
+                </Box>
+            </Box>
         </Box>
     )
 }

7b91741be26db8d20986b44c7c9d1e2d333820ba
Lucy Veron <lucymveron@gmail.com>
2023-01-31 15:22:46 +0100

    Line chart

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Invoices from './scenes/invoices';
 import Form from './scenes/form';
 import Calendar from './scenes/calendar';
 import Bar from './scenes/bar';
-// import Pie from './scenes/pie';
+import Pie from './scenes/pie';
 // import Line from './scenes/line';
 import Faq from './scenes/faq';
 // import Geography from './scenes/geography';
@@ -35,8 +35,8 @@ function App() {
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<Faq />} />
               <Route path="/bar" element={<Bar />} />
-              {/* 
               <Route path="/pie" element={<Pie />} />
+              {/* 
               <Route path="/line" element={<Line />} />
               <Route path="/geography" element={<Geography />} />
                */}
